d2b7a548b3aa986763b1b75d44abed349bca9fc0
Yashaswini-09-07 <yashaswini092004@gmail.com>
2025-05-14 18:59:08 +0530

    histogram.py

--- histogram.py
+++ histogram.py
@@ -1,13 +1,31 @@
 #HISTOGRAM CALCULATION AND EQUALIZATION 
 import cv2
-# Read the image in grayscale
+import matplotlib.pyplot as plt
+
+# Load image in grayscale
 img = cv2.imread('image.jpg', 0)
 
 # Histogram equalization
 equalized = cv2.equalizeHist(img)
 
-# Show original and equalized images
+# Calculate histograms
+hist_original = cv2.calcHist([img], [0], None, [256], [0, 256])
+hist_equalized = cv2.calcHist([equalized], [0], None, [256], [0, 256])
+
+# Show images
 cv2.imshow('Original', img)
 cv2.imshow('Equalized', equalized)
+
+# Show histograms
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1)
+plt.plot(hist_original)
+plt.title('Original Histogram')
+
+plt.subplot(1, 2, 2)
+plt.plot(hist_equalized)
+plt.title('Equalized Histogram')
+plt.show()
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
